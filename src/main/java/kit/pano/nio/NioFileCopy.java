5fb36b7ddac66e3003b4c0bfd0d262c7979635bf
Hibernate Pano <362472407@qq.com>
2020-06-02 10:54:12 +0800

    FileChannel 文件操作

--- src/main/java/kit/pano/nio/NioFileCopy.java
+++ src/main/java/kit/pano/nio/NioFileCopy.java
@@ -4,7 +4,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Objects;
 
 /**
@@ -12,16 +16,18 @@ import java.util.Objects;
  *
  * @author pano
  */
-public class NioChannelFileCopy {
+public class NioFileCopy {
 
     public static void main(String[] args) throws IOException {
         String fileIn = "/Users/pano/Pictures/4o5dy7.jpg";
         String fileOut1 = "/Users/pano/Pictures/out1.jpg";
         String fileOut2 = "/Users/pano/Pictures/out2.jpg";
         String fileOut3 = "/Users/pano/Pictures/out3.jpg";
-        copyFileThroughStream(fileIn, fileOut1);
-        copyFileThroughFileChannel(fileIn, fileOut2);
-        copyFileThroughFileChannelAndBuffer(fileIn, fileOut3);
+        String fileOut4 = "/Users/pano/Pictures/out4.jpg";
+//        copyFileThroughStream(fileIn, fileOut1);
+//        copyFileThroughFileChannel(fileIn, fileOut2);
+//        copyFileThroughFileChannelAndBuffer(fileIn, fileOut3);
+        copyFileThroughAddressMap(fileIn, fileOut4);
     }
 
 
@@ -36,10 +42,8 @@ public class NioChannelFileCopy {
         while ((len = Objects.requireNonNull(fileInputStream).read(buffer)) > 0) {
             fileOutputStream.write(buffer, 0, len);
         }
-//        fileInputStream.close();
-//        fileOutputStream.close();
-
-        //TODO 经测试，可以不关闭流和channel，考虑是否有影响。
+        fileInputStream.close();
+        fileOutputStream.close();
     }
 
     /**
@@ -51,18 +55,16 @@ public class NioChannelFileCopy {
         FileChannel inChannel = fileInputStream.getChannel();
         FileChannel outChannel = fileOutputStream.getChannel();
         outChannel.transferFrom(inChannel, 0, inChannel.size());
-//        inChannel.close();
-//        outChannel.close();
+        inChannel.close();
+        outChannel.close();
     }
 
     /**
      * 通过FileChannel复制文件
      */
     private static void copyFileThroughFileChannelAndBuffer(String fileIn, String fileOut) throws IOException {
-        FileInputStream fileInputStream = new FileInputStream(fileIn);
-        FileOutputStream fileOutputStream = new FileOutputStream(fileOut);
-        FileChannel inChannel = fileInputStream.getChannel();
-        FileChannel outChannel = fileOutputStream.getChannel();
+        FileChannel inChannel = new FileInputStream(fileIn).getChannel();
+        FileChannel outChannel = new FileOutputStream(fileOut).getChannel();
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         while (inChannel.read(byteBuffer) > 0) {
             //转换到读模式
@@ -72,7 +74,27 @@ public class NioChannelFileCopy {
             //清楚缓存，切换到写模式
             byteBuffer.clear();
         }
-//        inChannel.close();
-//        outChannel.close();
+        inChannel.close();
+        outChannel.close();
+    }
+
+    /**
+     * 使用内存映射文件的方式实现文件复制的功能(直接操作缓冲区)：
+     */
+    private static void copyFileThroughAddressMap(String fileIn, String fileOut) throws IOException {
+        FileChannel inChannel = FileChannel.open(Paths.get(fileIn), StandardOpenOption.READ);
+        FileChannel outChannel = FileChannel.open(Paths.get(fileOut), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE);
+
+        //内存映射文件
+        MappedByteBuffer inBuffer = inChannel.map(MapMode.READ_ONLY, 0, inChannel.size());
+        MappedByteBuffer outBuffer = outChannel.map(MapMode.READ_WRITE, 0, inChannel.size());
+
+        //设置数组长度读取buffer数据
+        byte[] dst = new byte[inBuffer.limit()];
+        inBuffer.get(dst);
+        outBuffer.put(dst);
+
+        inChannel.close();
+        outBuffer.clear();
     }
 }
